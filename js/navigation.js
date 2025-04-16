2f5faaeea34caa261e5eae2f8d2816d9fcfd5cdc
JB1qa <61047780+JB1qa@users.noreply.github.com>
2025-04-16 12:54:58 +0300

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,10 @@
+const hero = document.querySelector('#visit');
+
+window.addEventListener('scroll', function () {
+  const nav = document.querySelector('.navigation');
+  if (window.scrollY > visit.offsetHeight - 50) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
