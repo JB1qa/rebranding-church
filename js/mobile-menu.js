ba37c94245008e02b0cfb38bdf84374dd1738e47
JB1qa <61047780+JB1qa@users.noreply.github.com>
2025-04-27 10:45:59 +0300

    upd-fix-js-issue

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
 const menuContent = document.querySelector(".mobile-menu__content");
+const menuLinks = document.querySelectorAll(".mobile-menu a");
 const disableBody = () => document.body.classList.add("disable-hover");
 const enableBody = () => document.body.classList.remove("disable-hover");
 
@@ -10,13 +11,22 @@ const toggleMenu = () => {
 
   if (mobileMenu.classList.contains("is-open")) {
     disableBody();
+    document.body.style.overflow = "hidden";
   } else {
     enableBody();
+    document.body.style.overflow = "";
   }
 };
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open"); // Закрити меню при кліку на посилання
+    document.body.style.overflow = "";
+  });
+});
 
 document.addEventListener("click", function (e) {
   if (
@@ -25,6 +35,7 @@ document.addEventListener("click", function (e) {
     !menuBtnClose.contains(e.target)
   ) {
     mobileMenu.classList.remove("is-open");
+    document.body.style.overflow = "";
     enableBody();
   }
 });
